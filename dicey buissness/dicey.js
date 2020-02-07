b17919b6e79e47499631d989a7f2804ea5606940
Chase Graffeo <chasegraffeo@gmail.com>
2020-02-07 15:32:30 -0600

    moving to next lab

---
+++ dicey buissness/dicey.js
@@ -0,0 +1,14 @@
+/* document.addEventListener(`DOMContentLoaded`, function () {
+            let button = document.createElement(`button`);
+            let buttonText = document.createTextNode(`Generate Die`);
+            button.appendChild(buttonText);
+            document.body.appendChild(button);
+             document.addEventListner(`click`, function(){
+
+             });
+        })
+*/
+
+
+
+
