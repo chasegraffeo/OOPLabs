37027305d77f2ca9ddbc99cc6b6d60ac5a21e15f
Chase Graffeo <chasegraffeo@gmail.com>
2020-02-07 11:26:53 -0600

    extend to child

--- OOP.js
+++ OOP.js
@@ -1,4 +1,4 @@
-/* function Person(name,age,location){
+/* function Person(name,age,location){ //this is just a longer version of whats below
     this.name = name;
     this.age = age;
     this.location =location;
@@ -47,3 +47,61 @@ p4.sayHey();
 p5 = new Person(`Anna`,`29`,`Moody`);
 p5.sayHello();
 p5.sayHey();
+
+
+
+class Vehicle {
+    constructor(type, manfac, numwhls) {
+        this.type = type;
+        this.manfac = manfac;
+        this.numwhls = numwhls;
+    }
+    aboutVehicle() {
+        console.log(`I am a ${this.type}, ${this.manfac} is my manufacturer, this is my number of wheels ${this.numwhls}.`)
+    }
+}
+
+class Truck extends Vehicle {
+    constructor(type, manfac, numwhls, doors, truckBed) {
+        super(type, manfac, numwhls);
+        this.doors = doors;
+        this.truckBed = truckBed;
+    }
+    moreTruck() {
+        console.log(`I have ${this.doors} doors and ${this.truckBed} truckbed.`)
+    }
+}
+
+class Sedans extends Vehicle {
+    constructor(type, manfac, numwhls, size, mpg) {
+        super(type, manfac, numwhls);
+        this.size = size;
+        this.mpg = mpg;
+    }
+    moreSedans() {
+        console.log(`I am ${this.size} and I get ${this.mpg} mpg.`)
+    }
+}
+
+class Motorcycles extends Vehicle {
+    constructor(type, manfac, numwhls, bars, ndoors) {
+        super(type, manfac, numwhls);
+        this.bars = bars;
+        this.ndoors = ndoors;
+    }
+    moreMotor() {
+        console.log(`I have ${this.bars} and ${this.ndoors}`)
+    }
+}
+
+v1 = new Truck(`Colorado`, `Chevy`, `4`, `2`, `1`);
+v1.aboutVehicle();
+v1.moreTruck();
+
+v2 = new Sedans(`Galant`, `Mitsubishi`, `4`, `small`, `33`);
+v2.aboutVehicle();
+v2.moreSedans();
+
+v3 = new Motorcycles(`BSA`, `British Service & Arms`, `2`, `handlebars`, `no doors`);
+v3.aboutVehicle();
+v3.moreMotor();
