11a8a6eb8187fc0c0dd333b27060a5e3b5b9a3ce
Chase Graffeo <chasegraffeo@gmail.com>
2020-02-10 10:05:01 -0600

    finish version1

--- dicey buissness/dicey.js
+++ dicey buissness/dicey.js
@@ -1,29 +1,34 @@
-
 document.addEventListener(`DOMContentLoaded`, function () {
         let btn2 = document.getElementById(`generate`);
 
         let box = `box`;
         let sum = document.getElementById(`sum`);
+        let roll = document.getElementById(`roll`);
         btn2.addEventListener(`click`, function () {
                 let div = document.createElement(`div`);
-                let dtxt =document.createTextNode(Math.floor((Math.random() * 6) + 1));
+                let dtxt = document.createTextNode(Math.floor((Math.random() * 6) + 1));
                 div.appendChild(dtxt);
                 document.body.appendChild(div);
                 div.className = `square`;
                 div.id = box;
-               // document.getElementById(`box`).innerText = Math.floor((Math.random() * 6) + 1);
-                sum.addEventListener(`click`, function(){
-                        console.log(+dtxt + +dtxt)
+                div.addEventListener(`click`, function () {
+                        div.remove()
                 });
         });
 
+        sum.addEventListener(`click`, function () {
+                let diceArr = document.querySelectorAll(`.square`);
+                let sum = 0;
+                diceArr.forEach(die => {
+                        sum = sum + parseInt(die.innerText);
+                });
+                alert(sum);
+        });
+
+        roll.addEventListener(`click`, function () {
+                let diceArr = document.querySelectorAll(`.square`);
+                diceArr.forEach(die => {
+                        die.innerText = Math.floor((Math.random() * 6) + 1);
+                })
+        });
 });
-
-
-
-
-
-
-
-
-
